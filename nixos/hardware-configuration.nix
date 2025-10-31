77ccbb651a5da0bd83e02f37e1c3aa9255588577
Armin Babaei <armin.babaei@me.com>
2025-10-31 14:48:47 -0400

    Fix hardware config hostPlatform to aarch64-linux

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -27,6 +27,6 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
